d2f780240f0df4ad1748d1fd8fdc62b7348cda3c
till2194 <tilosepke@gmail.com>
2023-08-01 01:09:30 +0200

    First wifi and API test

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,110 @@
 #include <Arduino.h>
+#include <Arduino_JSON.h>
+#include <HTTPClient.h>
+#include <WiFi.h>
+
+#include "config.h"
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastTime = 0;
+// Timer set to 10 minutes (600000)
+// unsigned long timerDelay = 600000;
+// Set timer to 5 seconds (5000)
+unsigned long timerDelay = 5000;
+
+// Weather API
+const String openweatherServer = "https://api.openweathermap.org/data/3.0/onecall";
+const String serverPath = openweatherServer + "?lat=" + OPENWEATHER_LAT + "&lon=" + OPENWEATHER_LON + "&appid=" + OPENWEATHER_API_KEY;
+
+String weatherData;
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORT);
+
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+String httpGETRequest() {
+  HTTPClient http;
+
+  // Your Domain name with URL path or IP address with path
+  http.begin(serverPath.c_str());
+
+  // If you need Node-RED/server authentication, insert user and password below
+  // http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+
+  String payload = "{}";
+
+  if (httpResponseCode > 0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  } else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
+}
+
+void weatherUpdate() {
+  // Send an HTTP POST request every 10 minutes
+  if ((millis() - lastTime) > timerDelay) {
+    // Check WiFi connection status
+    if (WiFi.status() == WL_CONNECTED) {
+      weatherData = httpGETRequest();
+      Serial.println(weatherData);
+
+      JSONVar myObject = JSON.parse(weatherData);
+
+      // JSON.typeof(jsonVar) can be used to get the type of the var
+      if (JSON.typeof(myObject) == "undefined") {
+        Serial.println("Parsing input failed!");
+        return;
+      }
+
+      Serial.print("JSON object = ");
+      Serial.println(myObject);
+
+      // myObject.keys() can be used to get an array of all the keys in the object
+      JSONVar keys = myObject.keys();
+
+      for (int i = 0; i < keys.length(); i++) {
+        JSONVar value = myObject[keys[i]];
+        Serial.print(keys[i]);
+        Serial.print(" = ");
+        Serial.println(value);
+      }
+    } else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+}
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  delay(1000);
+
+  // Set WiFi to station mode
+  initWiFi();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  delay(200);
-
-  digitalWrite(LED_BUILTIN, LOW);
-
-  delay(1000);
+  weatherUpdate();
 }
